5c0e70a6882719747e281fdf04fff9f7ad9fdb1d
vascomadrid <vmadrid0426@gmail.com>
2020-06-26 12:40:38 -0700

    created write load data tool

---
+++ build/write_load_data.py
@@ -0,0 +1,6 @@
+def write_load_data(file_name, delim, table_name):
+    print("LOAD DATA LOCAL INFILE " + file_name)
+    print("INTO TABLE " + table_name)
+    print("FIELDS TERMINATED BY " + delim)
+    print("LINES TERMINATED BY \'\n\';")
+
