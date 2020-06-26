f2ba950402e455234fe7c0617d361d7223113939
clayscode <hcnorris@email.sc.edu>
2020-06-26 15:49:39 -0400

    created sql script with using methods in python scripts

---
+++ build/build_tags.sql
@@ -0,0 +1,87 @@
+CREATE TABLE tag245 {
+	col_id varchar(100),
+	col_1 varchar(100),
+	col_4 varchar(100),
+	col_5 varchar(100),
+	col_6 varchar(100),
+	col_a varchar(100),
+	col_b varchar(100),
+	col_c varchar(100),
+	col_d varchar(100),
+	col_e varchar(100),
+	col_f varchar(100),
+	col_g varchar(100),
+	col_h varchar(100),
+	col_i varchar(100),
+	col_k varchar(100),
+	col_l varchar(100),
+	col_m varchar(100),
+	col_n varchar(100),
+	col_o varchar(100),
+	col_p varchar(100),
+	col_q varchar(100),
+	col_r varchar(100),
+	col_s varchar(100),
+	col_t varchar(100),
+	col_u varchar(100),
+	col_v varchar(100),
+	col_w varchar(100),
+	col_x varchar(100),
+};
+LOAD DATA LOCAL INFILE "245.tsv"
+INTO TABLE tag245
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n';
+CREATE TABLE tag100 {
+	col_id varchar(100),
+	col_1 varchar(100),
+	col_4 varchar(100),
+	col_6 varchar(100),
+	col_a varchar(100),
+	col_b varchar(100),
+	col_c varchar(100),
+	col_d varchar(100),
+	col_e varchar(100),
+	col_f varchar(100),
+	col_g varchar(100),
+	col_h varchar(100),
+	col_j varchar(100),
+	col_k varchar(100),
+	col_l varchar(100),
+	col_n varchar(100),
+	col_p varchar(100),
+	col_q varchar(100),
+	col_s varchar(100),
+	col_t varchar(100),
+	col_u varchar(100),
+	col_v varchar(100),
+	col_x varchar(100),
+};
+LOAD DATA LOCAL INFILE "100.tsv"
+INTO TABLE tag100
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n';
+CREATE TABLE tag260 {
+	col_id varchar(100),
+	col_3 varchar(100),
+	col_5 varchar(100),
+	col_6 varchar(100),
+	col_a varchar(100),
+	col_b varchar(100),
+	col_c varchar(100),
+	col_d varchar(100),
+	col_e varchar(100),
+	col_f varchar(100),
+	col_g varchar(100),
+	col_m varchar(100),
+	col_n varchar(100),
+	col_p varchar(100),
+	col_s varchar(100),
+	col_v varchar(100),
+	col_x varchar(100),
+	col_z varchar(100),
+};
+LOAD DATA LOCAL INFILE "260.tsv"
+INTO TABLE tag260
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n';
--- build/wct_tags.py
+++ build/wct_tags.py
@@ -1,9 +1,14 @@
 from write_create_table import write_create_table
+from write_load_data import write_load_data
 
 file_path = "../../../../data/wc_data/fsplit/"
 tags = ["245", "100", "260"]
 
 for tag in tags:
-    write_create_table(file_path+ tag+'.tsv', '\t', 'tag' + tag, col_prefix='col_')
-
+    filename = tag + '.tsv'
+    delim_char = '\t'
+    delim_name = "\'\\t\'"
+    tablename = 'tag' + tag
+    write_create_table(file_path + filename, delim_char, tablename, col_prefix='col_')
+    write_load_data(filename, delim_name, tablename)
 
