5d8f4e8fd529a5c88318bcacb8a5a259fbf85f98
Vasco Madrid <44776047+vascomadrid@users.noreply.github.com>
2020-06-29 22:40:38 -0400

    Create create_term_table.sql

---
+++ queries/create_term_table.sql
@@ -0,0 +1,5 @@
+CREATE table terms_and_titles AS
+SELECT tag245.id, tag245.$a, terms.term_lc, terms.term_key, terms.language
+FROM tag245, terms
+WHERE tag245.$a LIKE(CONCAT(' ',terms.term_lc,' '))
+	OR tag245.$a LIKE(CONCAT(' ',terms.term_cap1,' ')); 
