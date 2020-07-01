24dbdaa72b6512f6e683ce08fc7cf86571974f45
Vasco Madrid <44776047+vascomadrid@users.noreply.github.com>
2020-06-30 22:31:29 -0400

    Update basicqs_on_terms_and_titles.sql

--- queries/basicqs_on_terms_and_titles.sql
+++ queries/basicqs_on_terms_and_titles.sql
@@ -4,12 +4,17 @@ SELECT COUNT(*) num_rows FROM terms_and_titles;
 SELECT term_lc, COUNT(id) 
 FROM terms_and_titles
 GROUP BY term_lc;
-# get number of total terms
-SELECT COUNT(*) num_all_terms FROM terms;
+# get total number of unique terms
+SELECT COUNT(term_lc) num_unique_terms 
+FROM (
+	SELECT term_lc 
+    FROM terms
+    GROUP BY term_lc
+    ) AS unique_terms;
 # get number of terms found in titles during search
 SELECT COUNT(*) num_found_terms
 FROM (
-	SELECT COUNT(*)
+	SELECT term_lc
 	FROM terms_and_titles
 	GROUP BY term_lc
     ) AS found_terms;
@@ -19,8 +24,25 @@ FROM terms
 WHERE term_lc NOT IN (
     SELECT term_lc
 	FROM terms_and_titles
-	GROUP BY term_lc ); 
+	GROUP BY term_lc)
+GROUP BY term_lc; 
 # Get title count per language
 SELECT language, count(id)
 FROM terms_and_titles
 GROUP BY language;
+
+# Get number of titles with more than one language
+SELECT COUNT(id), MAX(num_languages), AVG(num_languages)
+FROM (
+	SELECT id, COUNT(language) AS num_languages 
+	FROM terms_and_titles
+	GROUP BY id
+) AS id_groups
+WHERE num_languages > 1;
+# get total number of unique ids
+SELECT count(id) num_unique_ids
+FROM (
+	SELECT id
+    FROM terms_and_titles
+    GROUP BY id
+    ) AS unique_ids;
