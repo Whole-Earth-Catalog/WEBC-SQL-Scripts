6bbc26ef13fa5cf43895d34b2b9fa067c1309e5a
vascomadrid <vmadrid0426@gmail.com>
2020-06-30 22:59:01 -0400

    update queries

--- build/XMLTagToTSV.py
+++ build/XMLTagToTSV.py
@@ -1,5 +1,19 @@
 import xml.etree.ElementTree as ET
 
-file_path = "../../../../"
-tree = ET.parse('dr59.xml')
-root = tree.getroot()
+file_path = "../../../../../backup/data/wc_data/"
+file_name = "dr59.xml"
+
+tsv_file = open("008.tsv", 'w')
+tsv_file.write("id\tdata\n")
+with open(file_path + file_name, 'r') as f :
+    for line in f:
+	tree = ET.fromstring(line)
+	id = ""
+	tag008 = ""
+	for child in tree:
+	    if str(child.attrib) == "{\'tag\': \'001\'}":
+	        id = child.text
+	    elif str(child.attrib) == "{\'tag\': \'008\'}":
+		tag008 = child.text
+	tsv_file.write(id + "\t" + tag008 + "\n")
+ 
