cf1b12939951d9a6bcc832567402f87430778c44
Vasco Madrid <44776047+vascomadrid@users.noreply.github.com>
2020-08-02 16:08:56 -0400

    Update terms_by_decade.sql

--- queries/terms_by_decade.sql
+++ queries/terms_by_decade.sql
@@ -1,18 +1,19 @@
 use webc;
-SELECT concat(term_lc) AS term, concat(term_key) AS term_key, concat(language) AS language, CONCAT(decade, '0') as decade,COUNT(id) AS num_ids
+# get terms per decade
+SELECT concat(term_lc) AS term, concat(term_key) AS term_key, concat(language) AS language, CONCAT(decade_num, '0') as decade, COUNT(id) AS num_ids
 FROM (
-	SELECT terms_and_titles.*, SUBSTRING(tag008.data, 8, 3) AS decade
+	SELECT terms_and_titles.*, SUBSTRING(tag008.data, 8, 3) AS decade_num
     FROM terms_and_titles, tag008
     WHERE terms_and_titles.id=tag008.id
 ) AS terms_and_decades
-GROUP BY term_lc, term_key, decade, language
+GROUP BY term_lc, term_key, decade_num, language
 order by term_lc, decade;
-
-select concat(term_key) as term_key , CONCAT(decade, '0') as decade,COUNT(id) AS num_ids
+# get key_terms per decade and language
+select concat(term_key) as term_key , concat(language) AS language, CONCAT(decade_num, '0') as decade,COUNT(id) AS num_ids
 from (
-	SELECT terms_and_titles.*, SUBSTRING(tag008.data, 8, 3) AS decade
+	SELECT terms_and_titles.*, SUBSTRING(tag008.data, 8, 3) AS decade_num
     FROM terms_and_titles, tag008
     WHERE terms_and_titles.id=tag008.id
 ) AS terms_and_decades
-group by term_key, decade
+group by term_key, decade_num, language
 order by term_key, decade
