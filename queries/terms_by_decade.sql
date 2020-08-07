3c52b4d41306b565e040c86c9dfe860096798747
Vasco Madrid <44776047+vascomadrid@users.noreply.github.com>
2020-08-07 19:55:27 -0400

    Update terms_by_decade.sql

--- queries/terms_by_decade.sql
+++ queries/terms_by_decade.sql
@@ -1,10 +1,6 @@
 use webc;
 # get key_terms per decade and language
-select concat(term_key) as term_key , concat(language) AS language, CONCAT(decade_num, '0') as decade,COUNT(id) AS num_ids
-from (
-	SELECT terms_and_titles.*, SUBSTRING(tag008.data, 8, 3) AS decade_num
-    FROM terms_and_titles, tag008
-    WHERE terms_and_titles.id=tag008.id
-) AS terms_and_decades
-group by term_key, decade_num, language
-order by term_key, decade_num;
+select concat(term_key) as term_key , concat(language) AS language, CONCAT(decade, '0') as decade,COUNT(id) AS num_ids
+from terms_and_titles
+group by term_key, decade, language
+order by term_key, language, decade;
