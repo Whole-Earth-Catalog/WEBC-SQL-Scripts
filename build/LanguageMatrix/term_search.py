31ade57bf29a1f3b5d50352e9c3cd4963a0ce6a1
vascomadrid <vmadrid0426@gmail.com>
2020-08-09 17:10:12 -0400

    update

--- build/LanguageMatrix/term_search.py
+++ build/LanguageMatrix/term_search.py
@@ -16,25 +16,24 @@ def fill_keys(key_list):
         row_list.append(elem)
     return row_list
 
+def insert_terms(term_lc, term_key, term_type, language):
+    db_cursor.execute("INSERT INTO terms " + 
+                   "VALUES (\"" + term_lc +
+                   "\", \"" + term_key + 
+                   "\", \"" + term_type +
+                   "\", \"" + language + "\");")
+    webc_db.commit()
+
 gc = gspread.service_account()
 
 # Open language matrix sheet
+print("Opening spreadsheet...")
 lm = gc.open_by_key('1X9Ifq6mgzT0G-yjJPBoi1MVWh4KCc4qAQUfatY8rekE')
 # select worksheet
 lm_sheet = lm.get_worksheet(0)
-
-# connect to webc database
-print("Connecting to database ...")
-webc_db = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "root",
-    database = "webc"
-)
 print("Done.\n")
-# create database cursor
-db_cursor = webc_db.cursor()
 
+print("Getting values...")
 # Get a map of keys
 keys = lm_sheet.col_values(1)
 key_rows = fill_keys(keys)
@@ -49,20 +48,40 @@ columns = all_values[0]
 num_col = len(all_values[0])
 # print("number of columns: " + str(num_col))
 # print("columns: " + str(columns))
+print("Done.\n")
 
+# connect to webc database
+print("Connecting to database ...")
+webc_db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "root",
+    database = "webc"
+)
+print("Done.\n")
+# create database cursor
+db_cursor = webc_db.cursor()
+
+print("Dropping terms table if it exists")
 # remove terms table if it exists
-db_cursor.execute("drop table if exists terms")
+db_cursor.execute("drop table if exists terms;")
+webc_db.commit()
+print("Done.\n")
 
 # write create table command for full term table
 create_statement = ("CREATE TABLE terms (" + 
-      "term_lc varchar(40), " 
+      "term varchar(40), " 
       "term_key varchar(20), " 
       "term_type varchar(20), " 
       "language varchar(20));")
 
+print("Creating terms table")
 # create term table
 db_cursor.execute(create_statement)
+webc_db.commit()
+print("Done.\n")
 
+print("Inserting search term values into table...")
 for row in range(1,num_row):
     for col in range(1,num_col-1):
 	term = clean_item(all_values[row][col])
@@ -72,9 +91,8 @@ for row in range(1,num_row):
 	    language = columns[col]
 	    term_key = key_rows[row]
 	    # insert row into database
-	    db_cursor.execute("INSERT INTO terms " + 
-                   "VALUES (\"" + term_lc +
-                   "\", \"" + term_key + 
-                   "\", \"" + term_type +
-                   "\", \"" + language + "\");")
+	    insert_terms(term_lc, term_key, term_type, language)
+print("Done.\n")
 
+print("Inserting common term values into table...")
+print("Done.\n")
