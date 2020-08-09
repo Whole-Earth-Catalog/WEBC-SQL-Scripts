b2d2ba1ff29a6627b663301251dcd22475309c6a
Vasco Madrid <44776047+vascomadrid@users.noreply.github.com>
2020-08-09 17:35:39 -0400

    Update create_terms_and_titles.sql

--- build/LanguageMatrix/create_terms_and_titles.sql
+++ build/LanguageMatrix/create_terms_and_titles.sql
@@ -1,5 +1,5 @@
-drop table if exists terms_and_titles;
 # function to remove punctuation and capital letters in titles
+drop function if exists clean_title;
 delimiter //
 create function clean_title(title varchar(6000))
 returns varchar(6000)
@@ -16,15 +16,15 @@ begin
     return new_title;
 end//
 delimiter ;
-
+drop table if exists terms_and_titles;
 CREATE table terms_and_titles AS
-SELECT master_help.id, master_help.title, terms.term_lc, terms.term_key, terms.language, master_help.decade
+SELECT master_help.id, master_help.title, terms.term, terms.term_key, terms.language, master_help.decade, terms.term_type
 FROM master_help, terms
 WHERE (
 	clean_title(master_help.title) 
-		LIKE(CONCAT('% ',terms.term_lc,' %')) # term within title
+		LIKE(CONCAT('% ',terms.term,' %')) # term within title
     OR clean_title(master_help.title) 
-		LIKE(CONCAT(terms.term_lc,' %')) # term at beginning of title
+		LIKE(CONCAT(terms.term,' %')) # term at beginning of title
     OR clean_title(master_help.title) 
-		LIKE(CONCAT('% ',terms.term_lc)) # term at end of title
+		LIKE(CONCAT('% ',terms.term)) # term at end of title
     ) AND master_help.lang=lower(substring(terms.language,1,3)); 
