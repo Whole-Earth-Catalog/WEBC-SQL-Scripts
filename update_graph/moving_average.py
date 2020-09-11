2c1cf4553e52aff5efdd59f59ef1447b8458de13
vascomadrid <vmadrid0426@gmail.com>
2020-09-11 11:33:52 -0400

    update

---
+++ update_graph/moving_average.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import csv
+
+# get data from common_terms.tsv
+decades = []
+counts = []
+languages = []
+with open('common_terms.tsv') as tsv_file:
+    tsv_reader = csv.reader(tsv_file, delimiter='\t')
+    next(tsv_reader)
+    for row in tsv_reader:
+        decades.append(row[3])
+        counts.append(row[4])
+        languages.append(row[2])
+
+data_dict = {'decade':decades, 'count':counts, 'language':languages}
+# create pandas dataframe from data
+terms_df = pd.DataFrame(data_dict)
+new_terms_df = pd.DataFrame()
+# get a list of all languages (without duplicates)
+all_languages = list(set(languages))
+# perform EMA for each language
+for language in all_languages:
+    # get df for language
+    lang_df = terms_df.loc[terms_df['language']== language]
+    # add EMA values to lang_df
+    lang_df['EMA'] = lang_df.iloc[:,0].ewm(span=40,adjust=False).mean()
+    new_terms_df = pd.concat([new_terms_df,lang_df])
+    print(lang_df)
+
+print(new_terms_df)
+
+new_terms_df.to_csv('ema_data.csv', index=False)
